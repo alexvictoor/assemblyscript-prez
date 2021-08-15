ee13235eaa3eaed81cd4441c6141eb0f530e2ac5
Alex Victoor <alexvictoor@gmail.com>
2021-08-15 23:35:49 +0200

    conclusion

--- src/slides/4_perf/perf_7.js
+++ src/slides/4_perf/perf_7.js
@@ -5,21 +5,9 @@ export default (
   <Slide
     transition={["fade"]}
     bgColor="secondary"
-    bgImage="assets/chess.jpg"
-    bgDarken="0.5"
+    bgImage="assets/histogram-bench.png"
   >
-    <Heading size={1} fit caps lineHeight={1} textColor="primary">
-      bench perft
-    </Heading>
-    <Appear>
-      <Text textColor="tertiary"  >position initiale, 5 niveaux, 4865609 nodes</Text>
-    </Appear>
-    <Appear>
-      <Text textColor="tertiary" bold >JavaScript : 14674ms</Text>
-    </Appear>
-    <Appear>
-      <Text textColor="tertiary" bold >AssemblyScript : 1219ms</Text>
-    </Appear>
+    
     <Notes></Notes>
   </Slide>
 );
--- src/slides/5_end/conclusion.js
+++ src/slides/5_end/conclusion.js
@@ -1,15 +1,12 @@
 import React from "react";
-import { Heading, Notes, Slide, List, ListItem, Link, Text } from "spectacle";
+import { Heading, Notes, Slide, List, ListItem, Link, Text, Image } from "spectacle";
 
 export default (
     <Slide transition={['zoom']} textFont="secondary">
                      <Heading size={2} caps>
            THE END
           </Heading>
-          <List >
-            <ListItem margin="30px">TODO</ListItem>
-          
-          </List>
+          <Image src="assets/assemblyscript-qrcode.svg" />
           <Text bold textColor="tertiary">@Alex_Victoor</Text>
         
           <Notes>
