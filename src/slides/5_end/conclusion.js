61e33c966cfe0b4ad0e406b7bd383cf178c8bbf0
Alex Victoor <alexvictoor@gmail.com>
2022-04-19 21:22:25 +0200

    Improve last slide

--- src/slides/5_end/conclusion.js
+++ src/slides/5_end/conclusion.js
@@ -11,15 +11,15 @@ export default (
     </Heading>
     <Layout style={{ marginBottom: '50px' }}>
       <Fill style={{ textAlign: 'left'}} >
-        <Text textSize="30" bold >WebAssembly</Text>
-        <Text textSize="20"><a href="https://www.figma.com/fr/blog/webassembly-cut-figmas-load-time-by-3x/">Figma is powered by WebAssembly</a></Text>
-        <Text textSize="20" ><a href="https://developer.mozilla.org/en-US/docs/WebAssembly/Understanding_the_text_format">Doc Mozilla sur WAT</a></Text>
-        <Text textSize="20" ><a href="https://exercism.org/tracks/wasm">Track Exercism WASM/WAT</a></Text>
-        <Text textSize="30" bold >AssemblyScript</Text>
-        <Text textSize="20" ><a href="https://colineberhardt.github.io/wasm-mandelbrot/#WebAssembly">Bench Mandelbrot</a></Text>
-        <Text textSize="20" ><a href="https://github.com/alexvictoor/assemblyscript-hello-world">Hello world TS/AS</a></Text>
-        <Text textSize="20" ><a href="https://surma.dev/things/js-to-asc/index.html">Is WebAssembly magic performance pixie dust?</a></Text>
-        <Text textSize="20" ><a href="https://www.youtube.com/watch?v=u0Jgz6QVJqg&ab_channel=GoogleChromeDevelopers">AssemblyScript - HTTP 203</a></Text>
+        <Text textSize="40" bold >WebAssembly</Text>
+        <Text textSize="30"><a href="https://www.figma.com/fr/blog/webassembly-cut-figmas-load-time-by-3x/">Figma is powered by WebAssembly</a></Text>
+        <Text textSize="30" ><a href="https://developer.mozilla.org/en-US/docs/WebAssembly/Understanding_the_text_format">Doc Mozilla sur WAT</a></Text>
+        <Text textSize="30" ><a href="https://exercism.org/tracks/wasm">Track Exercism WASM/WAT</a></Text>
+        <Text textSize="40" bold >AssemblyScript</Text>
+        <Text textSize="30" ><a href="https://colineberhardt.github.io/wasm-mandelbrot/#WebAssembly">Bench Mandelbrot</a></Text>
+        <Text textSize="30" ><a href="https://github.com/alexvictoor/assemblyscript-hello-world">Hello world TS/AS</a></Text>
+        <Text textSize="30" ><a href="https://surma.dev/things/js-to-asc/index.html">Is WebAssembly magic performance pixie dust?</a></Text>
+        <Text textSize="30" ><a href="https://www.youtube.com/watch?v=u0Jgz6QVJqg&ab_channel=GoogleChromeDevelopers">AssemblyScript - HTTP 203</a></Text>
       </Fill>
       <Fill>
         <Image src="assets/assemblyscript-qrcode.svg" />
